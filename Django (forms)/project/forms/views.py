b58c5fd82a18b968a6bae4837652cc308a4efa65
Aykhanko Kerimli <150331571+aykhanko@users.noreply.github.com>
2024-12-11 12:24:25 +0400

    Update views.py

--- Django (forms)/project/forms/views.py
+++ Django (forms)/project/forms/views.py
@@ -5,23 +5,20 @@ def homepage(request):
     return render(request, "homepage.html")
 
 def quiz_view(request):
-    questions = Question.objects.all()  # Tüm soruları al
-    if request.method == 'POST':  # Eğer form gönderildiyse
-        correct_answers = 0  # Doğru cevap sayısı
-        incorrect_answers = []  # Yanlış cevaplar listesi
+    questions = Question.objects.all() 
+    if request.method == 'POST':  
+        correct_answers = 0  
+        incorrect_answers = []  
 
-        # Kullanıcının verdiği cevapları kontrol et
+        
         for question in questions:
-            # Kullanıcının seçtiği cevabı al
             answer_id = request.POST.get(f'question_{question.id}')
-            if answer_id:  # Eğer kullanıcı cevap verdiyse
+            if answer_id:  
                 answer = Answer.objects.get(id=answer_id)
-
-                # Eğer cevap doğruysa doğru sayısını artır
+                
                 if answer.is_correct:
                     correct_answers += 1
                 else:
-                    # Yanlış cevabı ve doğru cevabı tut
                     correct_answer = question.answers.filter(is_correct=True).first()
                     incorrect_answers.append({
                         'question': question.text,
